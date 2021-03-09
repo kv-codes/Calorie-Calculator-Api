87f23e0e864a88f2c2a160dae03d0eaa073c2f7e
kv-codes <lekevin98@gmail.com>
2021-03-08 19:51:09 -0600

    line 23 changed back to react-client/build

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'react-client/build')));
 
 //morgan
 app.use(logger('dev'));
